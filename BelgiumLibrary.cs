23b47173a3c3f4aa72ac68b84f014811a0721106
Mazurkiewicz Aliocha <contact@aliocha.be>
2024-05-09 11:14:26 +0200

    Init

---
+++ BelgiumLibrary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Bridge
+{
+    public class BelgiumLibrary
+    {
+        private static String fileName;
+
+        public static String ModuleFileName
+        {
+            get
+            {
+                if (fileName == null)
+                {
+                    if (OperatingSystem.IsMacOS())
+                    {
+                        fileName = "/Library/Belgium Identity Card/Pkcs11/beid-pkcs11.bundle/Contents/MacOS/libbeidpkcs11.dylib";
+                    }
+                    if (OperatingSystem.IsLinux())
+                    {
+                        if (OperatingSystem.IsX64())
+                        {
+                            fileName = "/usr/lib/x86_64-linux-gnu/libbeidpkcs11.so.0";
+                        }
+                        else
+                        {
+                            fileName = "libbeidpkcs11.so";
+
+                        }
+                    }
+                    if (OperatingSystem.IsWindows())
+                    {
+                        fileName = "beidpkcs11.dll";
+                    }
+                }
+
+                return fileName;
+            }
+            set
+            {
+            }
+        }
+
+    }
+}
